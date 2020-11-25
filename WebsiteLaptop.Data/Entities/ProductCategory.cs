7f38efd94603c1675a2adb92a9f92acdf514847c
Le Minh <minhcrazy97@gmail.com>
2020-11-25 21:41:18 +0700

    complete create database 11

---
+++ WebsiteLaptop.Data/Entities/ProductCategory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebsiteLaptop.Data.Enums;
+using WebsiteLaptop.Data.Interfaces;
+using WebsiteLaptop.Infrastructure.SharedKernel;
+
+namespace WebsiteLaptop.Data.Entities
+{
+    public class ProductCategory : DomainEntity<int>, ISwitchable, ISortable, IDateTracking
+    {
+        public ProductCategory()
+        {
+            Products = new List<Product>();
+        }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int? ParentId { get; set; }
+
+        public int? HomeOrder { get; set; }
+
+        public string Image { get; set; }
+
+        public bool? HomeFlag { get; set; }
+
+        public DateTime DateCreated { set; get; }
+        public DateTime DateModified { set; get; }
+        public int SorOrder { set; get; }
+        public Status Status { set; get; }
+
+        public virtual ICollection<Product> Products { set; get; }
+    }
+}
