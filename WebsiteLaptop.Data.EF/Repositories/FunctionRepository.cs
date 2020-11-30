334c09864eb9b72b5ee9ea3343edf20574b08c46
LeMinh97 <minhcrazy97@gmail.com>
2020-11-30 18:08:12 +0700

    continue 18m

---
+++ WebsiteLaptop.Data.EF/Repositories/FunctionRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebsiteLaptop.Data.Entities;
+using WebsiteLaptop.Data.IRepositories;
+
+namespace WebsiteLaptop.Data.EF.Repositories
+{
+    public class FunctionRepository : EFRepository<Function, string>, IFunctionRepository
+    {
+        public FunctionRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ WebsiteLaptop.Service/Interfaces/IFunctionService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebsiteLaptop.Service.ViewModels.System;
+
+namespace WebsiteLaptop.Service.Interfaces
+{
+    public interface IFunctionService : IDisposable
+    {
+        Task<List<FunctionViewModel>> GetAll();
+
+        List<FunctionViewModel> GetAllByPermission(Guid userId);
+    }
+}
