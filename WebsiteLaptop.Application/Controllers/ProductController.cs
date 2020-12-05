f73d0ff4ffb20bd05c0205fe0d52eb755050abcd
Le Minh <minhcrazy97@gmail.com>
2020-12-05 15:20:20 +0700

    completed 46

--- WebsiteLaptop.Application/Controllers/ProductController.cs
+++ WebsiteLaptop.Application/Controllers/ProductController.cs
@@ -2,12 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Mvc;
+using WebsiteLaptop.Service.Interfaces;
+using WebsiteLaptop.Application.Models.ProductViewModels;
 
 namespace WebsiteLaptop.Application.Controllers
 {
     public class ProductController : Controller
     {
+        IProductService _productService;
+        IProductCategoryService _productCategoryService;
+        IConfiguration _configuration;
+        public ProductController(IProductService productService, IConfiguration configuration,
+            IProductCategoryService productCategoryService)
+        {
+            _productService = productService;
+            _productCategoryService = productCategoryService;
+            _configuration = configuration;
+        }
+
         [Route("products.html")]
         public IActionResult Index()
         {
@@ -15,7 +29,23 @@ namespace WebsiteLaptop.Application.Controllers
         }
 
         [Route("{alias}-c.{id}.html")]
-        public IActionResult Catalog(int id, string keyword, int? pageSize, string sortBy, int page = 1)
+        public IActionResult Catalog(int id, int? pageSize, string sortBy, int page = 1)
+        {
+            var catalog = new CatalogViewModel();
+            ViewData["BodyClass"] = "shop_grid_full_width_page";
+            if (pageSize == null)
+                pageSize = _configuration.GetValue<int>("PageSize");
+
+            catalog.PageSize = pageSize;
+            catalog.SortType = sortBy;
+            catalog.Data = _productService.GetAllPaging(id, string.Empty, page, pageSize.Value);
+            catalog.Category = _productCategoryService.GetById(id);
+
+            return View(catalog);
+        }
+
+        [Route("{alias}-p.{id}.html", Name = "ProductDetail")]
+        public IActionResult Details(int id)
         {
             return View();
         }
